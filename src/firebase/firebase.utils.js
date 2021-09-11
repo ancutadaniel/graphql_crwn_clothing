a771d52d7fcacefb222057157f6360f3ceadad58
Daniel Ancuta <ancutadaniel@gmail.com>
2021-09-11 11:47:32 +0300

    update components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyBOTrNJjBcOFIcryl0PPVCYg3a4q9BCV9k',
+  apiKey: '',
   authDomain: 'crown-db-ce483.firebaseapp.com',
   projectId: 'crown-db-ce483',
   storageBucket: 'crown-db-ce483.appspot.com',
